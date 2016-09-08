7e4ed55698d19c0636a729fdcc31abbc00a9452f
Rhys Moyne <rhysmoyne@users.noreply.github.com>
2016-09-08 10:50:41 +0930

    Update travis-install.sh

--- travis-install.sh
+++ travis-install.sh
@@ -4,4 +4,5 @@ git clone https://gitlab.com/rhysmoyne/school-website.git && \
 cd school-website && \
 git submodule init && \
 git submodule update && \
+git submodule update --remote content/projects && \
 git status
